bbd874c4112b6bf9c31c591314ae89a6be1432aa
JeanDuarte00 <jean_luccas320@hotmail.com>
2023-01-21 09:21:58 -0300

    adding project

---
+++ sugundaSemana/src/main/java/com/eldorado/sugundaSemana/SugundaSemanaApplication.java
@@ -0,0 +1,13 @@
+package com.eldorado.sugundaSemana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugundaSemanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SugundaSemanaApplication.class, args);
+	}
+
+}
